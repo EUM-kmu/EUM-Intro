e56d4debc1c4819a0970e7c7cf7216c70e319e0f
Jiwon Choi <jwchoi@lablup.com>
2023-11-21 01:10:40 +0900

    feat: `TopBar` component 구현

---
+++ src/components/TopBar/index.jsx
@@ -0,0 +1,12 @@
+import { Container } from "./styles";
+import { ReactComponent as Logo } from "../../assets/logo.svg";
+
+const TopBar = () => {
+  return (
+    <Container>
+      <Logo />
+    </Container>
+  );
+};
+
+export default TopBar;
---
+++ src/components/TopBar/styles.jsx
@@ -0,0 +1,9 @@
+import { styled } from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  padding: 28px 30px;
+  width: 100%;
+  background: white;
+  position: sticky;
+`;

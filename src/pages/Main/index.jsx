0a2db03ce025f701e2a6819132bde8ed51eef935
Jiwon Choi <jwchoi@lablup.com>
2023-11-22 19:20:32 +0900

    feat: Intro 첫 페이지 UI 임시 배치 및 관련 파일 추가

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,11 +1,57 @@
 import TopBar from "../../components/TopBar";
-import { Container } from "./styles";
+import identity from "../../assets/identity.svg";
+import icon from "../../assets/app_icon.svg";
+import title from "../../assets/app_title.svg";
+import phone1 from "../../assets/phone_1.svg";
+import phone2 from "../../assets/phone_2.svg";
+import { Container, FlexBox, ImageBox, Text } from "./styles";
 
 const Main = () => {
   return (
     <>
       <TopBar />
-      <Container />
+      <Container color={"#ffa339"}>
+        <FlexBox
+          flexdirection={"column"}
+          alignitems={"center"}
+          justifycontent={"space-between"}
+        >
+          <FlexBox gap={"33px"}>
+            <ImageBox src={icon} />
+            <ImageBox src={title} />
+          </FlexBox>
+          <Text textalign={"center"} fontSize={"20px"} fontWeight={500}>
+            우리 마을 주민들의 니즈를 반영한 동네 소통 플랫폼, 햇살마을이에요.
+            <br />
+            햇살마을에서 지역 이웃들과의 교류를 시작해 볼까요?
+          </Text>
+          <FlexBox gap={"44px"}>
+            <ImageBox src={phone1} />
+            <ImageBox src={phone2} />
+          </FlexBox>
+        </FlexBox>
+      </Container>
+
+      {/* <Container>
+        <FlexBox
+          flexDirection={"column"}
+          justifyContent={"center"}
+          alignItems={"center"}
+        >
+          <Text
+            fontSize={"20px"}
+            fontWeight={500}
+            color={"var(--palette-systemgray-07, #8E8E93)"}
+            textAlign={"center"}
+          >
+            우리 마을 주민들의 니즈를 반영한 동네 소통 플랫폼, 햇살마을이에요.
+            <br />
+            햇살마을에서 지역 이웃들과의 교류를 시작해 볼까요?
+          </Text>
+          <ImageBox width={"250px"} height={"250px"} src={identity} />
+        </FlexBox>
+      </Container> */}
+
       <Container />
       <Container />
     </>
--- src/pages/Main/styles.jsx
+++ src/pages/Main/styles.jsx
@@ -3,10 +3,27 @@ import { styled } from "styled-components";
 export const Text = styled.p`
   font-size: ${(props) => props.fontSize};
   font-weight: ${(props) => props.fontWeight};
+  text-align: ${(props) => props.textalign};
+  color: ${(props) => props.color};
 `;
 
 export const Container = styled.div`
   width: 100vw;
   height: 100vh;
-  background: #f2f2f7;
+  background: ${(props) => props.color || "#f2f2f7"};
+`;
+
+export const FlexBox = styled.div`
+  display: flex;
+  gap: ${(props) => props.gap};
+  flex-direction: ${(props) => props.flexdirection};
+  justify-content: ${(props) => props.justifycontent};
+  align-items: ${(props) => props.alignitems};
+`;
+
+export const ImageBox = styled.img.attrs({
+  alt: "이미지 로드 실패",
+})`
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
 `;

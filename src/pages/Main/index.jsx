2d9085c4a59db3d47d91ae4e39af8b2607aad90f
Jiwon Choi <jwchoi@lablup.com>
2023-11-23 11:46:30 +0900

    feat: Screen 2 구성 요소 UI 추가 (배치 X)

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,16 +1,18 @@
 import TopBar from "../../components/TopBar";
-import identity from "../../assets/identity.svg";
 import icon from "../../assets/app_icon.svg";
 import title from "../../assets/app_title.svg";
 import phone1 from "../../assets/phone_1.svg";
 import phone2 from "../../assets/phone_2.svg";
-import { FlexBox, ImageBox, Text } from "./styles";
+import empty from "../../assets/empty_circle.svg";
+// import filled from "../../assets/filled_circle.svg";
+import phoneProfile from "../../assets/phone_profile.svg";
+import bgProfile from "../../assets/bg_profile_web.png";
 
 const Main = () => {
   return (
     <>
       <TopBar />
-      {/* Screen 1 */}
+      {/** Screen 1 */}
       <div className="flex flex-col pt-12 w-screen h-[1080px] items-center justify-between bg-[#FFA339]">
         <div className="flex flex-col">
           <div className="flex gap-8">
@@ -29,58 +31,30 @@ const Main = () => {
         </div>
       </div>
 
-      {/* Screen 2 */}
-      <FlexBox
-        padding={"48px 0px 0px 0px"}
-        width={"100vw"}
-        height={"calc(100vh - 140px)"}
-        color={"var(--primary)"}
-        flexdirection={"column"}
-        alignitems={"center"}
-        justifycontent={"space-between"}
-      >
-        <FlexBox flexdirection={"column"}>
-          <FlexBox gap={"33px"}>
-            <ImageBox src={icon} />
-            <ImageBox src={title} />
-          </FlexBox>
-          <Text
-            padding={"24px 0px"}
-            textalign={"center"}
-            fontSize={"20px"}
-            fontWeight={500}
-            color={"white"}
-          >
-            우리 마을 주민들의 니즈를 반영한 동네 소통 플랫폼, 햇살마을이에요.
-            <br />
-            햇살마을에서 지역 이웃들과의 교류를 시작해 볼까요?
-          </Text>
-        </FlexBox>
-        <FlexBox gap={"44px"}>
-          <ImageBox src={phone1} />
-          <ImageBox src={phone2} />
-        </FlexBox>
-      </FlexBox>
+      {/** Screen 2 */}
 
-      {/* <Container>
-        <FlexBox
-          flexDirection={"column"}
-          justifyContent={"center"}
-          alignItems={"center"}
-        >
-          <Text
-            fontSize={"20px"}
-            fontWeight={500}
-            color={"var(--palette-systemgray-07, #8E8E93)"}
-            textAlign={"center"}
-          >
-            우리 마을 주민들의 니즈를 반영한 동네 소통 플랫폼, 햇살마을이에요.
-            <br />
-            햇살마을에서 지역 이웃들과의 교류를 시작해 볼까요?
-          </Text>
-          <ImageBox width={"250px"} height={"250px"} src={identity} />
-        </FlexBox>
-      </Container> */}
+      {/** Navigator */}
+      <div className="relative flex flex-col w-screen h-[1080px] items-center bg-[#F2F2F7]">
+        <div className="flex font-2xl font-bold gap-5 items-center">
+          <p className="text-4xl font-bold text-[#042D53]">ONBOARDING</p>
+          <div className="flex gap-2">
+            <img src={empty} />
+            <img src={empty} />
+            <img src={empty} />
+            <img src={empty} />
+            <img src={empty} />
+          </div>
+        </div>
+
+        <p className="text-4xl text-[#042D53] font-bold z-10">프로필 생성</p>
+        <p className="z-10 text-xl font-medium text-center text-[#7E93A3] ">
+          단계를 따라 프로필을 생성하고 연령대별
+          <br />
+          캐릭터를 선택할 수 있어요.
+        </p>
+        <img src={phoneProfile} />
+        <img className="absolute z-0 left-0" src={bgProfile} />
+      </div>
     </>
   );
 };
--- src/pages/Main/styles.jsx
+++ src/pages/Main/styles.jsx
@@ -6,10 +6,12 @@ export const Text = styled.p`
   text-align: ${(props) => props.textalign};
   color: ${(props) => props.color};
   padding: ${(props) => props.padding};
+  z-index: ${(props) => props.zIndex};
 `;
 
 export const FlexBox = styled.div`
   display: flex;
+  position: ${(props) => props.position};
   padding: ${(props) => props.padding};
   width: ${(props) => props.width};
   height: ${(props) => props.height};
@@ -18,11 +20,24 @@ export const FlexBox = styled.div`
   flex-direction: ${(props) => props.flexdirection};
   justify-content: ${(props) => props.justifycontent};
   align-items: ${(props) => props.alignitems};
+  z-index: ${(props) => props.zIndex};
 `;
 
 export const ImageBox = styled.img.attrs({
   alt: "이미지 로드 실패",
 })`
+  position: ${(props) => props.position};
   width: ${(props) => props.width};
   height: ${(props) => props.height};
+  z-index: ${(props) => props.zIndex};
+
+  /* relative, absolute 사용 시에만 아래 속성을 적용합니다. */
+  ${(props) =>
+    props.position === "absolute" &&
+    `
+    top: ${props.top};
+    bottom: ${props.bottom};
+    left: ${props.left};
+    right: ${props.right};
+  `}
 `;

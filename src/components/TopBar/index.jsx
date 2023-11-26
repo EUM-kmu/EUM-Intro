898e3279585bc77b153a1d0ea01e1340bcdd2df3
Jiwon Choi <jwchoi@lablup.com>
2023-11-26 20:19:32 +0900

    refactor: `TopBar` 반응형 적용 및 styled-components 제거

--- src/components/TopBar/index.jsx
+++ src/components/TopBar/index.jsx
@@ -1,12 +1,10 @@
-import { Container } from "./styles";
 import logo from "/src/assets/logo.svg";
 
 const TopBar = () => {
   return (
-    <Container>
-      <img src={logo} />
-      
-    </Container>
+    <div className="flex sm:h-[60] px-2 sm:px-[30] py-2 sm:py-3 w-screen bg-white sticky top-0 z-30">
+      <img className="h-3 sm:h-9" src={logo} />
+    </div>
   );
 };
 

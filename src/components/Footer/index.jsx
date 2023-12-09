ebbfda082cfab9320e4e72596394545219accd96
Jiwon Choi <jwchoi@lablup.com>
2023-12-09 09:51:58 +0900

    feat: 서비스 이용약관, 개인정보 보호방침 페이지 연결

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,13 +4,15 @@ const Footer = () => {
       <p className="text-[6.75px] sm:text-2xl font-bold">이음</p>
       <div className="flex gap-1 sm:gap-4">
         <a
-          href="#"
+          target="_blank"
+          href="https://achieved-crawdad-960.notion.site/191b1734f32840258d7c113fc6b53858?pvs=4"
           className="underline text-[4.5px] sm:text-base text-[#8E8E93] cursor-pointer"
         >
           서비스 이용약관
         </a>
         <a
-          href="#"
+          target="_blank"
+          href="https://achieved-crawdad-960.notion.site/2cbd879e8f0147139b3f9ee7a5b6b9dd?pvs=4"
           className="underline text-[4.5px] sm:text-base text-[#8E8E93] cursor-pointer border-b-[#8E8E93]"
         >
           개인정보 보호방침

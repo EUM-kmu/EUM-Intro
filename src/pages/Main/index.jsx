c156dece69726a24f33761ee4c2e14fae9114f51
Jiwon Choi <jwchoi@lablup.com>
2023-12-09 18:55:25 +0900

    feat: 소개 영상 홈페이지 삽입

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -23,6 +23,7 @@ import qrApple from "../../assets/qr.png";
 import qrGoogle from "../../assets/qr.png";
 import Footer from "../../components/Footer";
 import postFlow from "../../assets/post_flow.svg";
+import ReactPlayer from "react-player";
 
 const Main = () => {
   return (
@@ -45,6 +46,26 @@ const Main = () => {
           <img className="w-[116px] sm:w-96" src={phone1} />
           <img className="w-[116px] sm:w-96" src={phone2} />
         </div>
+      </div>
+
+      <div className="flex justify-center">
+        <ReactPlayer
+          className="react-player"
+          url={
+            "https://kr.object.ncloudstorage.com/k-eum/ppt/%EC%98%81%EC%83%81.mp4"
+          } // 플레이어 url
+          width="800px" // 플레이어 크기 (가로)
+          height="500px" // 플레이어 크기 (세로)
+          playing={true} // 자동 재생 on
+          muted={true} // 자동 재생 on
+          controls={true} // 플레이어 컨트롤 노출 여부
+          light={false} // 플레이어 모드
+          pip={true} // pip 모드 설정 여부
+          poster={
+            "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg"
+          } // 플레이어 초기 포스터 사진
+          onEnded={() => handleVideo()} // 플레이어 끝났을 때 이벤트
+        />
       </div>
 
       {/** Screen 2 */}

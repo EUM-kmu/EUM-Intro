00b94f1737e4dd9e2c530c4d18a9d2537dcbe618
Jiwon Choi <jwchoi@lablup.com>
2023-12-10 23:03:32 +0900

    chore: `react-health-check` 적용

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,30 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import GlobalStyles from "./GlobalStyle.jsx";
 import "./index.css";
+import { HealthCheckConfig } from "@webscopeio/react-health-check";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <>
+  <HealthCheckConfig
+    value={{
+      services: [
+        {
+          name: "healthCheck",
+          url: "/health",
+        },
+      ],
+      onSuccess: ({ service, timestamp }) => {
+        console.log(
+          `Service "${service.name}" is available since "${timestamp}" 🎉`
+        );
+      },
+      onError: ({ service, timestamp }) => {
+        console.log(
+          `Service "${service.name}" is not available since "${timestamp}" 😔`
+        );
+      },
+    }}
+  >
     <GlobalStyles />
     <App />
-  </>
+  </HealthCheckConfig>
 );

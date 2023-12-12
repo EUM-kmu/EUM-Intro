b736d4815eb857c421647f0d9bba126994fd8c8e
Jiwon Choi <jwchoi@lablup.com>
2023-12-12 23:21:31 +0900

    design: Mobile 환경에서의 fontWeight, fontSize 수정

--- src/components/StoreContainer/inedx.jsx
+++ src/components/StoreContainer/inedx.jsx
@@ -3,14 +3,10 @@ const StoreContainer = ({ qrSrc, iconSrc, text, path }) => {
     <div className="flex flex-col gap-2 sm:gap-9 items-center">
       <img className="w-7 sm:w-[106px]" src={qrSrc} />
       <a href={path} target="_blank">
-        <div className="w-24 sm:w-[343px] flex sm:gap-2 justify-center items-center bg-[#042D53] py-2 sm:py-4 rounded sm:rounded-xl cursor-pointer">
+        <div className="w-24 sm:w-[343px] flex gap-1 sm:gap-2 justify-center items-center bg-[#042D53] py-2 sm:py-4 rounded sm:rounded-xl cursor-pointer">
           {" "}
-          {iconSrc ? (
-            <img className="w-[6.75px] sm:w-6" src={iconSrc} />
-          ) : (
-            <></>
-          )}
-          <p className="text-white text-[5.65px] sm:text-xl font-bold">
+          {iconSrc ? <img className="w-[12px] sm:w-6" src={iconSrc} /> : <></>}
+          <p className="text-white text-[9px] sm:text-xl font-semibold sm:font-bold">
             {text}
           </p>
         </div>

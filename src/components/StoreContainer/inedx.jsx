6e965897a5dd938cc3d78d96d89a8e9e58fa6039
Jiwon Choi <jwchoi@lablup.com>
2023-12-12 22:23:33 +0900

    fix: `iconSrc` props가 존재하지 않을 경우 공간 차지 안하도록 수정

--- src/components/StoreContainer/inedx.jsx
+++ src/components/StoreContainer/inedx.jsx
@@ -3,7 +3,7 @@ const StoreContainer = ({ qrSrc, iconSrc, text }) => {
     <div className="flex flex-col gap-2 sm:gap-12 items-center">
       <img className="w-7 sm:w-[106px]" src={qrSrc} />
       <div className="w-24 sm:w-[343px] flex sm:gap-2 justify-center items-center bg-[#042D53] py-2 sm:py-4 rounded sm:rounded-xl cursor-pointer">
-        <img className="w-[6.75px] sm:w-6" src={iconSrc} />
+        {iconSrc ? <img className="w-[6.75px] sm:w-6" src={iconSrc} /> : <></>}
         <p className="text-white text-[5.65px] sm:text-xl font-bold">{text}</p>
       </div>
     </div>

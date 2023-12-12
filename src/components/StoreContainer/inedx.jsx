0fb50c28f2cdecc98a6a7829c4328c1676def696
Jiwon Choi <jwchoi@lablup.com>
2023-12-12 23:16:33 +0900

    feat: `StoreContainer`에 a 태그 추가, 사이트 이동 가능하도록 구현

--- src/components/StoreContainer/inedx.jsx
+++ src/components/StoreContainer/inedx.jsx
@@ -1,11 +1,20 @@
-const StoreContainer = ({ qrSrc, iconSrc, text }) => {
+const StoreContainer = ({ qrSrc, iconSrc, text, path }) => {
   return (
     <div className="flex flex-col gap-2 sm:gap-9 items-center">
       <img className="w-7 sm:w-[106px]" src={qrSrc} />
-      <div className="w-24 sm:w-[343px] flex sm:gap-2 justify-center items-center bg-[#042D53] py-2 sm:py-4 rounded sm:rounded-xl cursor-pointer">
-        {iconSrc ? <img className="w-[6.75px] sm:w-6" src={iconSrc} /> : <></>}
-        <p className="text-white text-[5.65px] sm:text-xl font-bold">{text}</p>
-      </div>
+      <a href={path} target="_blank">
+        <div className="w-24 sm:w-[343px] flex sm:gap-2 justify-center items-center bg-[#042D53] py-2 sm:py-4 rounded sm:rounded-xl cursor-pointer">
+          {" "}
+          {iconSrc ? (
+            <img className="w-[6.75px] sm:w-6" src={iconSrc} />
+          ) : (
+            <></>
+          )}
+          <p className="text-white text-[5.65px] sm:text-xl font-bold">
+            {text}
+          </p>
+        </div>
+      </a>
     </div>
   );
 };

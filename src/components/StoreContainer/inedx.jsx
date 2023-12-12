cc643f8ea2c433161d4c3685d8c4f98ae7319621
Jiwon Choi <jwchoi@lablup.com>
2023-12-12 22:41:15 +0900

    design: `StoreContainer`의 QR 이미지와 이동 버튼 사이 gap 조정

--- src/components/StoreContainer/inedx.jsx
+++ src/components/StoreContainer/inedx.jsx
@@ -1,6 +1,6 @@
 const StoreContainer = ({ qrSrc, iconSrc, text }) => {
   return (
-    <div className="flex flex-col gap-2 sm:gap-12 items-center">
+    <div className="flex flex-col gap-2 sm:gap-9 items-center">
       <img className="w-7 sm:w-[106px]" src={qrSrc} />
       <div className="w-24 sm:w-[343px] flex sm:gap-2 justify-center items-center bg-[#042D53] py-2 sm:py-4 rounded sm:rounded-xl cursor-pointer">
         {iconSrc ? <img className="w-[6.75px] sm:w-6" src={iconSrc} /> : <></>}
